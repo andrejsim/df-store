c9b83afa78bc24b67d304a73a37d703e0dc0f16a
Andrej Mihajlovski <andrej.mihajlovski@meteogroup.com>
2019-01-17 10:49:25 +0100

    basic go container with aws infra

---
+++ docker.build.goinfra.sh
@@ -0,0 +1,1 @@
+docker build -t goinfra:v1 -f Dockerfile.example .
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+func main(){
+	// come out of package b and then go inside package a to run the executable file as
+	cmd := exec.Command("../a/a.go")
+	if err := cmd.Run(); err != nil{
+		fmt.Println(err)
+	}
+}
+
